a58ba3afe4a6ddc795819138aa0180abbc54d8c5
Md Sawjal Sikder <134833280+Sawjal-sikder@users.noreply.github.com>
2024-05-09 21:50:08 +0600

    Delete catagories directory

--- catagories/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import catagories,laptop,phone
-urlpatterns = [
-    path('', catagories, name='catagories'),
-    path('laptop/', laptop, name='laptop'),
-    path('phone/', phone, name='phone'),
-]
